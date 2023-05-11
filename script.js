17e2cb2f94fb243c3d1c508e2ddc2cb1a1193224
ErnestKnowing <e3050901@gmail.com>
2023-05-11 15:37:00 +0300

    feat:add from popup entrance

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+//Registration
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+  }
+
+  //Entrance
+  function openFormEntrance() {
+    document.getElementById("myFormEntrance").style.display = "block";
+  }
+  
+  function closeFormEntrance() {
+    document.getElementById("myFormEntrance").style.display = "none";
   }
